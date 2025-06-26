cbba12d094533e221fbc03a2b57354528f570ce5
Luke Lee <lukelee01@gmail.com>
2025-06-25 23:25:42 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='wrapper'>
+        <div className='header'>header</div>
+        <div className='main' >
+            {/*<div className='element-a'>element a</div>*/}
+            {/*<div className='element-b'>element b</div>*/}
+        </div>
+        <div className='footer' >footer</div>
+    </div>
+  )
+}
+
+export default App
